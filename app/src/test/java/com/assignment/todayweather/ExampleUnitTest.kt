e830f280f888f55e33a2e92d5ab366d80bc186e7
zontaya <zontaya@gmail.com>
2022-03-28 10:10:07 +0700

    update test

--- app/src/test/java/com/assignment/todayweather/ExampleUnitTest.kt
+++ app/src/test/java/com/assignment/todayweather/ExampleUnitTest.kt
@@ -1,11 +1,9 @@
 package com.assignment.todayweather
 
 
+import com.assignment.todayweather.data.remote.model.Coord
 import com.assignment.todayweather.di.mModules
-import com.assignment.todayweather.domain.interactors.GetDailyParams
-import com.assignment.todayweather.domain.interactors.GetDailyUseCase
-import com.assignment.todayweather.domain.interactors.SearchCityParams
-import com.assignment.todayweather.domain.interactors.SearchCityUseCase
+import com.assignment.todayweather.domain.interactors.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collectLatest
@@ -22,6 +20,7 @@ import kotlin.test.assertEquals
 @OptIn(ExperimentalCoroutinesApi::class)
 class ExampleUnitTest : KoinTest {
     private val searchCityUseCase: SearchCityUseCase by inject()
+    private val searchCityByLocationUseCase: SearchCityByLocationUseCase by inject()
     private val getDailyUseCase: GetDailyUseCase by inject()
 
     @BeforeTest
@@ -39,6 +38,16 @@ class ExampleUnitTest : KoinTest {
         )
         searchCityUseCase.execute(param).collectLatest {
             assertEquals(it.name.lowercase(), param.name)
+        }
+    }
+
+    @Test
+    fun testSearchCityByLocation() = runTest {
+        val param = SearchCityByLocationParams(
+            location = Coord(lat = 13.75, lon = 100.5167), units = "metric"
+        )
+        searchCityByLocationUseCase.execute(param).collectLatest {
+            assertEquals(it.sys.country, "TH")
         }
     }
 
